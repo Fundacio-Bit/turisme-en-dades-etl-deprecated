317a74866fa779877e1c98feb5e474679b15c236
eaguado <eaguado@fundaciobit.org>
2020-11-13 13:29:40 +0100

    Modified to access Rest API.

--- src/utils/axios_requests.js
+++ src/utils/axios_requests.js
@@ -4,33 +4,31 @@ const makePost = (baseUrl, dataObj) => {
   const headers = {
     "Content-Type": "application/json",
   };
-  axios
-    .post(
-      "http://54.77.111.120:5300/login",
-      {
-        username: "admin",
-        password: "Admin2020_",
-      },
-      { headers: headers }
-    )
-    .then(response => {
-      console.log('token:', response.data.token)
-      return axios({
-        method: 'post',
-        baseURL: baseUrl,
-        url: '/data-grids',
-        data: dataObj,
-        headers: { authorization: `Bearer ${response.data.token}` }
-      })
+  axios({
+    method: 'post',
+    baseURL: baseUrl,
+    url: '/login',
+    data: { username: 'admin', password: 'Admin2020_' },
+    headers: headers
+  })
+  .then(response => {
+    console.log('token:', response.data.token)
+    return axios({
+      method: 'post',
+      baseURL: baseUrl,
+      url: '/data-grids',
+      data: dataObj,
+      headers: { authorization: `Bearer ${response.data.token}` }
     })
-    .then(response => {
-      console.log(response.data)
-      process.exit(0)
-    })
-    .catch(err => {
-      console.log(err.toString())
-      process.exit(1)
-    })
+  })
+  .then(response => {
+    console.log(response.data)
+    process.exit(0)
+  })
+  .catch(err => {
+    console.log(err.toString())
+    process.exit(1)
+  })
 } 
 
 module.exports = { makePost };
