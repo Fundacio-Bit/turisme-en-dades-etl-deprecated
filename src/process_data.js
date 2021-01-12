d14aa820ab67058ca0df4a2275eea9b85fb2bbf3
eaguado <eaguado@fundaciobit.org>
2021-01-12 08:36:32 +0100

    Changes in excel sheet of November.

--- src/process_data.js
+++ src/process_data.js
@@ -18,54 +18,54 @@ const excelToJson = (inputFile, month, baseUrl) => {
             case sections[0]:
                 console.log(sheet)
                 var datos = XLSX.utils.sheet_to_json(excel.Sheets[sheet]);
-                // request.makePost(baseUrl, Tourists.touristArrivals(datos, month)); 
-                // request.makePost(baseUrl, Tourists.touristArrivalsAcc(datos, month));
-                // request.makePost(baseUrl, Tourists.spending(datos, month));
-                // request.makePost(baseUrl, Tourists.spendingAcc(datos, month));
-                // request.makePost(baseUrl, Tourists.spendingTotal(datos, month));
-                // request.makePost(baseUrl, Tourists.spendingTotalAcc(datos, month));
-                // request.makePost(baseUrl, Tourists.spendingStays(datos, month));
-                // request.makePost(baseUrl, Tourists.spendingStaysIslands(datos, month));
-                // request.makePost(baseUrl, Tourists.spendingStaysAcc(datos, month));
-                // request.makePost(baseUrl, Tourists.spendingStaysIslandsAcc(datos, month));
+                request.makePost(baseUrl, Tourists.touristArrivals(datos, month)); 
+                request.makePost(baseUrl, Tourists.touristArrivalsAcc(datos, month));
+                request.makePost(baseUrl, Tourists.spending(datos, month));
+                request.makePost(baseUrl, Tourists.spendingAcc(datos, month));
+                request.makePost(baseUrl, Tourists.spendingTotal(datos, month));
+                request.makePost(baseUrl, Tourists.spendingTotalAcc(datos, month));
+                request.makePost(baseUrl, Tourists.spendingStays(datos, month));
+                request.makePost(baseUrl, Tourists.spendingStaysIslands(datos, month));
+                request.makePost(baseUrl, Tourists.spendingStaysAcc(datos, month));
+                request.makePost(baseUrl, Tourists.spendingStaysIslandsAcc(datos, month));
                 break;
             case sections[1]:
                 console.log(sheet)
                 var datos = XLSX.utils.sheet_to_json(excel.Sheets[sheet]);
-                // request.makePost(baseUrl, Occupancy.occupancy(datos, month));
-                // request.makePost(baseUrl, Occupancy.occupancyAcc(datos, month));
+                request.makePost(baseUrl, Occupancy.occupancy(datos, month));
+                request.makePost(baseUrl, Occupancy.occupancyAcc(datos, month));
                 break;
             case sections[2]:
                 console.log(sheet)
                 var datos = XLSX.utils.sheet_to_json(excel.Sheets[sheet]);
-                // request.makePost(baseUrl, AirPassengers.air_passengers_arrivals(datos, month));
-                // request.makePost(baseUrl, AirPassengers.air_passengers_arrivals_acc(datos, month));
+                request.makePost(baseUrl, AirPassengers.air_passengers_arrivals(datos, month));
+                request.makePost(baseUrl, AirPassengers.air_passengers_arrivals_acc(datos, month));
                 break;
             case sections[3]:
                 console.log(sheet)
                 var datos = XLSX.utils.sheet_to_json(excel.Sheets[sheet]);
-                // request.makePost(baseUrl, SeaPassengers.cruisePassengersArrivalsAP(datos, month))
-                // request.makePost(baseUrl, SeaPassengers.cruisePassengersArrivalsP(datos, month))
-                // request.makePost(baseUrl, SeaPassengers.cruisePassengersArrivalsAccAP(datos, month))
-                // request.makePost(baseUrl, SeaPassengers.cruisePassengersArrivalsAccP(datos, month))
-                // request.makePost(baseUrl, SeaPassengers.seaPassengersArrivalsAP(datos, month))
-                // request.makePost(baseUrl, SeaPassengers.seaPassengersArrivalsP(datos, month))
-                // request.makePost(baseUrl, SeaPassengers.seaPassengersArrivalsAccAP(datos, month))
-                // request.makePost(baseUrl, SeaPassengers.seaPassengersArrivalsAccP(datos, month))
+                request.makePost(baseUrl, SeaPassengers.cruisePassengersArrivalsAP(datos, month))
+                request.makePost(baseUrl, SeaPassengers.cruisePassengersArrivalsP(datos, month))
+                request.makePost(baseUrl, SeaPassengers.cruisePassengersArrivalsAccAP(datos, month))
+                request.makePost(baseUrl, SeaPassengers.cruisePassengersArrivalsAccP(datos, month))
+                request.makePost(baseUrl, SeaPassengers.seaPassengersArrivalsAP(datos, month))
+                request.makePost(baseUrl, SeaPassengers.seaPassengersArrivalsP(datos, month))
+                request.makePost(baseUrl, SeaPassengers.seaPassengersArrivalsAccAP(datos, month))
+                request.makePost(baseUrl, SeaPassengers.seaPassengersArrivalsAccP(datos, month))
                 break;
             case sections[4]:
                 console.log(sheet)
                 var datos = XLSX.utils.sheet_to_json(excel.Sheets[sheet]);
-                // request.makePost(baseUrl, Ambiental.energy_demand(datos, month))
-                // request.makePost(baseUrl, Ambiental.human_pressure(datos, month))
+                request.makePost(baseUrl, Ambiental.energy_demand(datos, month))
+                request.makePost(baseUrl, Ambiental.human_pressure(datos, month))
                 break;
             case sections[5]:
                 console.log(sheet)
                 var datos = XLSX.utils.sheet_to_json(excel.Sheets[sheet]);
-                // request.makePost(baseUrl, Social.affiliates(datos, month));
+                request.makePost(baseUrl, Social.affiliates(datos, month));
                 request.makePost(baseUrl, Social.unemployed(datos, month));
                 request.makePost(baseUrl, Social.temporality(datos, month));
-                // request.makePost(baseUrl, Social.companies(datos, month));
+                request.makePost(baseUrl, Social.companies(datos, month));
                 break;
             default:
                 break;
